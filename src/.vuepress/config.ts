9fd7cf2caf7a3353dc72d211ea1281098f17bed5
didiplus <972479352@qq.com>
2023-02-04 15:09:08 +0800

    deploy

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blog/",
   lang: "zh-CN",
   description: "工作总结，读书笔记",
   theme,
